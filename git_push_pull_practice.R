4699d8c366b2408f320be08f21bf6c638cfa891d
deniko333] <enikodenes3@gmail.com>
2024-12-13 12:59:17 +0000

    notes added

--- git_push_pull_practice.R
+++ git_push_pull_practice.R
@@ -1,3 +1,13 @@
 library(RPostgres)
 library(tidyverse) # best stuff ever
 
+# steps for pushing and pulling
+
+# 1; save the file -> find the file name on the right, thick the square and commit
+# give a description to the commit otherwise it won't work
+#push the commited file 
+# 2;if conflict message, first pull then type in the console git config pull.rebase false
+# resolve the conflict (only leave the code you want to appear in the final version
+# delete the dahs lines and head and that long number thing)
+# 3; thick the file name again in the right (blue square U U) and commit the changes
+# 4; push the changes and check results on git (might have to refresh)
